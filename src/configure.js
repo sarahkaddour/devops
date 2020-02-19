fc27dfd4c352256389d01aac0d9c5398cf715afa
sergkudinov <sergkudinov@gmail.com>
2020-02-19 09:41:25 +0100

    configure: update

--- src/configure.js
+++ src/configure.js
@@ -7,9 +7,10 @@ module.exports = (config_user = {}) => {
   config = mixme.merge(config_default, config_user)
   if (config.users.db_dir)
     config.users.db_dir = path.join(__dirname, '..', config.users.db_dir)
-  
-  if (!config.users.db_dir){
-    fs.mkdirSync(config.users.db_dir);
-  }
+
+  // Create directory if not exists
+  if (!fs.existsSync(config.users.db_dir))
+    fs.mkdirSync(config.users.db_dir)
+
   return config
 }
